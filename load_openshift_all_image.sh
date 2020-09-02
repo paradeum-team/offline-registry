5e8f853f01c44833aa4c016259d92cc71e7acc03
liujinye <liujinye@hisuntech.com>
2020-09-02 14:42:21 +0800

    load_openshift_all_image.sh add load registry.paradeum.com.imagelist.txt

--- load_openshift_all_image.sh
+++ load_openshift_all_image.sh
@@ -7,5 +7,7 @@ PACKAGEING_TAG=${1:-stable}
 ./load_concurrent_registry_data.sh ../offline-okd/images_manage/docker.io.imagelist.txt docker.io &
 ./load_concurrent_registry_data.sh ../offline-okd/images_manage/quay.io.imagelist.txt quay.io &
 ./load_concurrent_registry_data.sh ../offline-okd/images_manage/registry.access.redhat.com.imagelist.txt registry.access.redhat.com &
+./load_concurrent_registry_data.sh ../offline-okd/images_manage/registry.paradeum.com.imagelist.txt registry.paradeum.com &
 
 wait
+

db057f324c14351c849746fcdc6f7daacd9d3e98
root <chen_chen@hisuntech.com>
2021-01-30 11:09:29 +0800

    修改为自动获取目录

--- pre.sh
+++ pre.sh
@@ -20,14 +20,18 @@ if [ -f $CONFIG_DIR/config.yml ];then
 fi
 cp $CONFIG_DIR/config.yml.templ $CONFIG_DIR/config.yml && \
 sed -i 's/--registry_ip--/'$LOCAL_IP'/g' $CONFIG_DIR/config.yml && \
-docker images|grep $registry_image_name || docker load -i ../offline-images/registry.tar.gz 
+docker images|grep $registry_image_name || docker load -i ../offline-registry-image/registry.tar.gz 
 
 mkdir -p ../offline-registry_data
 
 # 安装rhsm redhat-uep.pem, 解决pull radhat 镜像问题
-wget http://mirror.centos.org/centos/7/os/x86_64/Packages/python-rhsm-certificates-1.19.10-1.el7_4.x86_64.rpm
-rpm2cpio python-rhsm-certificates-1.19.10-1.el7_4.x86_64.rpm | cpio -iv --to-stdout ./etc/rhsm/ca/redhat-uep.pem > /etc/rhsm/ca/redhat-uep.pem
-rm -f python-rhsm-certificates-1.19.10-1.el7_4.x86_64.rpm
+#wget http://${CONFIGSERVER_IP}:${CONFIGSERVER_PORT}/packages/centos/base/x86_64/RPMS/python-rhsm-certificates-1.19.10-1.el7_4.x86_64.rpm
+#rpm2cpio python-rhsm-certificates-1.19.10-1.el7_4.x86_64.rpm | cpio -iv --to-stdout ./etc/rhsm/ca/redhat-uep.pem > /etc/rhsm/ca/redhat-uep.pem
+#rm -f python-rhsm-certificates-1.19.10-1.el7_4.x86_64.rpm
+
+#自动获取挂载目录
+mount_path1=$(dirname $(pwd))"/offline-registry_data"
+mount_path2=$BASE_DIR"/"$CONFIG_DIR
 
 docker stop offline-registry || echo
 docker rm offline-registry || echo
@@ -35,8 +39,8 @@ docker run -d --name offline-registry \
                         --privileged --restart=always \
                         --log-driver journald \
                         --net host \
-                        -v /home/banana/offline-registry_data:/var/lib/registry \
-                        -v /home/banana/offline-registry/config/registry:/etc/registry \
+                        -v $mount_path1:/var/lib/registry \
+                        -v $mount_path2:/etc/registry \
                         -e GODEBUG=netdns=cgo \
                         $registry_image_name serve /etc/registry/config.yml
 

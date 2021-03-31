902af354f37218d3e02806781991cdb04476556b
banana <18779181569@163.com>
2021-03-31 14:32:26 +0800

    下载目录中的所有镜像

--- load_all_image.sh
+++ load_all_image.sh
@@ -1,6 +1,14 @@
 BASE_DIR=$(cd `dirname $0` && pwd)
 cd $BASE_DIR
 
+IMAGELIST_DIR="../offline-imagelist"
+
 echo "all images download is starting..."
-./loadimage.sh ../offline-imagelist/images.properties
+
+for file in `ls $IMAGELIST_DIR`;
+do
+	filedir="$IMAGELIST_DIR/$file"
+	./loadimage.sh $filedir
+done
+
 
